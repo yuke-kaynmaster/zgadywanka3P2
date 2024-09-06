4d459c168bd800a013b3ca8fb08788b9670657f2
yuke-kaynmaster <qutaro008@gmail.com>
2024-09-06 09:48:28 +0200

    testowanie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("siemano!");
+        System.out.println("cos!");
+        System.out.println("gitara!");
     }
 }

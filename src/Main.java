33da39788137c5f3bc315e3745b31e82a5e3d664
yuke-kaynmaster <qutaro008@gmail.com>
2024-09-06 11:32:59 +0200

    haseleczko lol podro ez

--- src/Main.java
+++ src/Main.java
@@ -96,5 +96,25 @@ public class Main {
         }   }
         while (wpisanaLiczba != wylosowanaLiczba);
         System.out.println("wygrana");
+
+        //pierwsze zadanie domowe zadanie8
+        //wygeneruj haslo losowe. Długość hasła powinna wynosić 20 znaków, a ponadto powinno zawierać liczby, wielkie i małe litery oraz znaki specjalne.
+        String haslo ="";
+        String maleLitery = "qwertyuiopasdfghjklzxcvbnm";
+        String duzeLitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        String znakiSpecjalne = "!@#$%^&*()_-+=<>?,./`~[{]}|";
+        String cyfry = "1234567890";
+        int losowa;
+        for (int i = 0; i < 5; i++) {
+            losowa = (int) (Math.random()*maleLitery.length());
+            haslo = haslo + maleLitery.charAt(losowa);
+            losowa = (int) (Math.random()*duzeLitery.length());
+            haslo = haslo + duzeLitery.charAt(losowa);
+            losowa = (int) (Math.random()*znakiSpecjalne.length());
+            haslo = haslo + znakiSpecjalne.charAt(losowa);
+            losowa = (int) (Math.random()*cyfry.length());
+            haslo = haslo + cyfry.charAt(losowa);
+        }
+        System.out.println("Wygenerowane haslo: "+haslo);
    }
 }
